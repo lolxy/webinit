39c3c6ffea90cbaefcb7f016ff1235288fd3b816
lolxy <lolxy@users.noreply.github.com>
2018-01-29 23:17:02 +0800

    初始化
    初始化

---
+++ data（备份）/conf/config.php
@@ -0,0 +1,12 @@
+<?php	return array (
+  'SP_SITE_ADMIN_URL_PASSWORD' => '',
+  'SP_DEFAULT_THEME' => 'simplebootx',
+  'DEFAULT_THEME' => 'simplebootx',
+  'SP_ADMIN_STYLE' => 'flat',
+  'URL_MODEL' => '0',
+  'URL_HTML_SUFFIX' => '',
+  'COMMENT_NEED_CHECK' => 0,
+  'COMMENT_TIME_INTERVAL' => 60,
+  'MOBILE_TPL_ENABLED' => 1,
+  'HTML_CACHE_ON' => false,
+);
---
+++ data（备份）/conf/db.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'dxzj_db',
+    'DB_USER' => 'root',
+    'DB_PWD' => 'root',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'dx_',
+    //密钥
+    "AUTHCODE" => 'Nf8MBz4vLABivuSgpU',
+    //cookies
+    "COOKIE_PREFIX" => 'djOlrv_',
+);
